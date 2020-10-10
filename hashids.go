d55b6729b094b1aa22c4e7d6bdb75b08424ff3aa
Indra Saputra <flame.fire99@gmail.com>
2020-10-10 21:10:02 +0700

    redefine encode and decode interface

--- hashids.go
+++ hashids.go
@@ -13,11 +13,11 @@ type ID int64
 
 // Hash defines the contract to encode and decode the ID.
 type Hash interface {
-	// Encode encodes the ID into a string.
-	// The string generated is the result of Hashids algorithm.
-	Encode(ID) (string, error)
-	// Decode decodes the string into an ID.
-	Decode(string) (ID, error)
+	// Encode encodes the ID into a slice of byte.
+	// The slice of byte generated is the result of Hashids algorithm.
+	Encode(ID) ([]byte, error)
+	// Decode decodes the slice of byte into an ID.
+	Decode([]byte) (ID, error)
 }
 
 // HashID can be used to encode and decode hashids.
