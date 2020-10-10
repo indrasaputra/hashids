7d69ccfdc5a576da379a26b3c102850dea48e65e
Indra Saputra <flame.fire99@gmail.com>
2020-10-10 20:41:39 +0700

    create type ID

--- hashids.go
+++ hashids.go
@@ -1,1 +1,8 @@
 package hashids
+
+// ID represents a unique identifier.
+// It means to replace the old int64 as unique ID.
+// Using this type allows the int64 to be obfuscated
+// into a random string using the Hashids algorithm.
+// Read more about hashids in https://hashids.org/.
+type ID int64
