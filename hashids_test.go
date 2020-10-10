cb9e422de6a49dd263557b0cf2dd5db26c8e9cd4
Indra Saputra <flame.fire99@gmail.com>
2020-10-10 21:16:38 +0700

    test encode

--- hashids_test.go
+++ hashids_test.go
@@ -28,3 +28,29 @@ func TestNewHashID(t *testing.T) {
 		}
 	})
 }
+
+func TestHashID_Encode(t *testing.T) {
+	t.Run("successfully encodes uint64 into a slice of byte length at least the same as minimal length", func(t *testing.T) {
+		tables := []struct {
+			minLength uint
+			salt      string
+			id        hashids.ID
+		}{
+			{0, "salt", 1},
+			{1, "one-salt", 10},
+			{2, "]1=-3asc", 7},
+			{3, "~!/..&%%!(#", 193013},
+			{4, "|=\\//f35022fj!^@*H((&&#", 323652},
+		}
+
+		for _, table := range tables {
+			hash, _ := hashids.NewHashID(table.minLength, table.salt)
+			res, err := hash.Encode(table.id)
+
+			assert.Nil(t, err)
+			assert.NotNil(t, res)
+			assert.NotEmpty(t, res)
+			assert.True(t, int(table.minLength) <= len(res))
+		}
+	})
+}
