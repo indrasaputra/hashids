70a43aa2023e5d45d3b3f2eaf1b80769f3c4c450
Indra Saputra <flame.fire99@gmail.com>
2020-10-10 21:06:49 +0700

    test hashid constructor

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/indrasaputra/hashids
 
 go 1.15
 
-require github.com/speps/go-hashids v2.0.0+incompatible
+require (
+	github.com/speps/go-hashids v2.0.0+incompatible
+	github.com/stretchr/testify v1.6.1
+)
--- hashids_test.go
+++ hashids_test.go
@@ -1,1 +1,30 @@
 package hashids_test
+
+import (
+	"testing"
+
+	"github.com/indrasaputra/hashids"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewHashID(t *testing.T) {
+	t.Run("successfully create an instance of HashID", func(t *testing.T) {
+		tables := []struct {
+			minLength uint
+			salt      string
+		}{
+			{0, "salt"},
+			{1, "one-salt"},
+			{2, "]1=-3asc"},
+			{3, "~!/..&%%!(#"},
+			{4, "|=\\//f35022fj!^@*H((&&#"},
+		}
+
+		for _, table := range tables {
+			hash, err := hashids.NewHashID(table.minLength, table.salt)
+
+			assert.Nil(t, err)
+			assert.NotNil(t, hash)
+		}
+	})
+}
