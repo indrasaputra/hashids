6e4717999b9e1460c90e60fb4d32a38fe946ff2d
Indra Saputra <flame.fire99@gmail.com>
2020-10-11 11:58:04 +0700

    benchmark encode with various length

--- hashids_bench_test.go
+++ hashids_bench_test.go
@@ -19,3 +19,33 @@ func BenchmarkID_UnmarshalJSON(b *testing.B) {
 		id.UnmarshalJSON([]byte(`"J4r0MA20No"`))
 	}
 }
+
+func BenchmarkHashID_Encode(b *testing.B) {
+	b.Run("length: 5", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(5, "common-salt")
+		for i := 0; i < b.N; i++ {
+			hash.Encode(hashids.ID(i))
+		}
+	})
+
+	b.Run("length: 10", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(10, "common-salt")
+		for i := 0; i < b.N; i++ {
+			hash.Encode(hashids.ID(i))
+		}
+	})
+
+	b.Run("length: 15", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(15, "common-salt")
+		for i := 0; i < b.N; i++ {
+			hash.Encode(hashids.ID(i))
+		}
+	})
+
+	b.Run("length: 20", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(20, "common-salt")
+		for i := 0; i < b.N; i++ {
+			hash.Encode(hashids.ID(i))
+		}
+	})
+}
