0ab11f8b2ecb19ce514c74ba1fa13b6effdeab45
Indra Saputra <flame.fire99@gmail.com>
2020-10-11 12:00:03 +0700

    benchmark decode

--- hashids_bench_test.go
+++ hashids_bench_test.go
@@ -49,3 +49,37 @@ func BenchmarkHashID_Encode(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkHashID_Decode(b *testing.B) {
+	b.Run("length: 5", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(5, "common-salt")
+		res, _ := hash.Encode(100)
+		for i := 0; i < b.N; i++ {
+			hash.Decode(res)
+		}
+	})
+
+	b.Run("length: 10", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(10, "common-salt")
+		res, _ := hash.Encode(100)
+		for i := 0; i < b.N; i++ {
+			hash.Decode(res)
+		}
+	})
+
+	b.Run("length: 15", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(15, "common-salt")
+		res, _ := hash.Encode(100)
+		for i := 0; i < b.N; i++ {
+			hash.Decode(res)
+		}
+	})
+
+	b.Run("length: 20", func(b *testing.B) {
+		hash, _ := hashids.NewHashID(20, "common-salt")
+		res, _ := hash.Encode(100)
+		for i := 0; i < b.N; i++ {
+			hash.Decode(res)
+		}
+	})
+}
